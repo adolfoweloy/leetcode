41c1ef6b163658d87fe8a872c8cb8a0f1d568e8d
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2024-06-12 20:55:20 +1000

    ValidParentheses simplifies data structures needed

--- src/main/java/com/adolfoeloy/array/ValidParentheses.java
+++ src/main/java/com/adolfoeloy/array/ValidParentheses.java
@@ -1,11 +1,9 @@
 package com.adolfoeloy.array;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
 public class ValidParentheses {
-    private static final List<Character> closingBrackets = List.of(')', '}', ']');
     private static final Map<Character, Character> rules = Map.of(
             ')', '(',
             '}', '{',
@@ -14,9 +12,8 @@ public class ValidParentheses {
 
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        if (s.length() < 2) return false;
         for (char c : s.toCharArray()) {
-            if (closingBrackets.contains(c)) {
+            if (rules.containsKey(c)) {
                 if (stack.isEmpty()) return false;
                 char openingBracket = stack.pop();
                 if (rules.get(c) != openingBracket) {
